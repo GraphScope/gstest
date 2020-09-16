68f8aa5a5a5a49b31c0682b53d8075b69d50cff8
qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>
2020-09-16 15:13:20 +0800

    update k3 data

--- k3.v
+++ k3.v
@@ -1,4 +1,4 @@
-0 {"weight": 0, "name": "vertex-0"}
-1 {"weight": 1, "name": "vertex-1"}
-2 {"weight": 2, "name": "vertex-2"}
-3 {"weight": 2, "name1": "vertex-3"}
+[0, {"weight": 0, "name": "vertex-0"}]
+[1, {"weight": 1, "name": "vertex-1"}]
+[2, {"weight": 2, "name": "vertex-2"}]
+[3, {"weight": 2, "name1": "vertex-3"}]
