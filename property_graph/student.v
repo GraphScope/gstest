7edaeea74394815a2490935be55b36b31690233c
Neng Li <lnfjpt@gmail.com>
2020-09-25 10:44:58 +0800

    Add property graph for test (#8)
    * Add property graph for test
    
    * update property graph
    
    * Add edge with same label
    
    * update format
    
    * update data format
    
    * update test files
    
    * remove label column of e
    
    Co-authored-by: nengli.ln <nengli.ln@alibaba-inc.com>
    Co-authored-by: siyuan0322 <siyuan0322@gmail.com>

---
+++ property_graph/student.v
@@ -0,0 +1,11 @@
+student_id,name,lesson_nums,avg_score
+0,John,3,490.33
+1,David,2,164.5
+2,Tom,4,190.25
+3,Bruce,1,762.0
+4,Glancy,5,434.2
+5,Brandon,3,513.0
+6,Joslin,2,569.0
+7,Lucy,1,25.0
+8,Powell,4,308.0
+9,Jobs,2,87.0
